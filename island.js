13eef243f1475ffcce552f75b6da37ce669b24a9
Cory Stever <cstever@hotmail.com>
2023-01-06 17:41:00 -0600

    Get Neighbors Island Solved

--- island.js
+++ island.js
@@ -1,24 +1,47 @@
 function getNeighbors(row, col, matrix) {
-  
+  let neighbors = [];
   // Check top
+  if (row > 0 && matrix[row - 1][col] === 1) {
+    neighbors.push([row -1, col]);
+  }
   // Check top right
+  if (row > 0 && col < matrix[0].length - 1 && matrix[row - 1][col + 1] === 1) {
+    neighbors.push([row - 1, col + 1]);
+  }
   // Check right
+  if (col < matrix[0].length - 1 && matrix[row][col + 1] === 1) {
+    neighbors.push([row ,col + 1]);
+  }
   // Check bottom right
+  if (row < matrix.length - 1 && col < matrix[0].length - 1 && matrix[row + 1][col + 1] === 1) {
+    neighbors.push([row + 1, col + 1]);
+  }
   // Check bottom
+  if (row < matrix.length - 1 && matrix[row + 1][col] === 1) {
+    neighbors.push([row + 1, col]);
+  }
   // Check bottom left
+  if (row < matrix.length - 1 && col > 0 && matrix[row + 1][col - 1] === 1) {
+    neighbors.push([row + 1, col - 1]);
+  }
   // Check left
+  if (col > 0 && matrix[row][col - 1] === 1) {
+    neighbors.push([row, col - 1]);
+  }
   // Check top left
+  if (row > 0 && col > 0 && matrix[row - 1][col - 1]) {
+    neighbors.push([row - 1, col - 1])
+  }
   // Return neighbors
-  
-  // Your code here
+  return neighbors;
 }
 
 function countIslands(matrix) {
-  
+
   // Create a visited set to store visited nodes
   // Initialize count to 0
   // Iterate through all indices in matrix
-    // If an index contains a 1 and has not been visited, 
+    // If an index contains a 1 and has not been visited,
     // increment island count and start traversing neighbors
       // DO THE THING (increment island count by 1)
       // Initialize a stack with current index
@@ -31,18 +54,19 @@ function countIslands(matrix) {
             // Add neighbor to stack
             // Mark neighbor as visited
   // Return island count
-  
+
   // Your code here
 }
 
 // Uncomment the lines below for local testing
-// const matrix = [
-//                 [1,1,1,0,0],
-//                 [0,1,1,0,1],
-//                 [0,1,1,0,1]
-//               ]
+const matrix = [
+                [1,1,1,0,0],
+                [0,1,1,0,1],
+                [0,1,1,0,1]
+              ]
 
-// console.log(getNeighbors(1, 1, matrix)); // [[0, 0], [0, 1], [0, 2], [1, 2], [2, 1], [2, 2]]
+// [[0, 0], [0, 1], [0, 2], [1, 2], [2, 1], [2, 2]]
+console.log(getNeighbors(1, 1, matrix));
 // console.log(getNeighbors(2,4, matrix)) // [[1,4]]
 
 // const matrix2 = [
@@ -53,5 +77,12 @@ function countIslands(matrix) {
 
 // console.log(countIslands(matrix)) // 2
 // console.log(countIslands(matrix2)); // 3
+
+// matrix3 = [
+//   [0,0,1,0,0,1,1],
+//   [1,1,0,0,1,0,1],
+//   [0,0,0,1,0,0,1],
+//   [1,0,1,0,0,1,1],
+// ]
 
 module.exports = [countIslands, getNeighbors];
